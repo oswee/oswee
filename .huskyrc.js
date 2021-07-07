6e4356ddb04ef0d99119de3a912b0a1d66aa27bf
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-07-07 16:11:24 +0300

    chore(repo): disables old husky config

--- .huskyrc.js
+++
@@ -1,8 +0,0 @@
-const tasks = arr => arr.join(' && ')
-
-module.exports = {
-  hooks: {
-    'pre-commit': tasks(['yarn lint-staged']),
-    'commit-msg': tasks(['yarn commitlint -E HUSKY_GIT_PARAMS']),
-  },
-}
