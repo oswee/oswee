14919a12fac2f8702fc60bd751f632ba0aed04e6
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-08-19 16:55:35 +0300

    chore(lintstaged): [wip] adds ts examples

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,36 @@
+'use strict';
+
 module.exports = {
   '*.{ts,html,md}': ['prettier --write'],
   '*.ts': ['eslint --fix'],
   // '.*{rc,.json}': ['jsonlint  --in-place'],
 }
+
+// TypeScrip try
+
+// const prettier = [
+//   'prettier --write'
+// ];
+
+// const eslint = [
+//   'eslint --fix'
+// ];
+
+// const json = [
+//   'jsonlint --in-place'
+// ];
+
+// type Command = string;
+
+// type Config = {
+//   [file in string]: Command[];
+// };
+
+// const config: Config = {
+//   '{.*,*}.{ts,html}': prettier,
+//   '*.ts': eslint,
+//   '*.md': prettier,
+//   '{.*,*}.{js,json}': json,
+// };
+
+// export default config;
