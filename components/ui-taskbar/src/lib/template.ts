0015ac69c138c5e72cc142a216bc3843fe4421ee
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-09-02 16:02:07 +0300

    chore(lit): ditch lit-element in favor of lit

--- components/ui-taskbar/src/lib/template.ts
+++ components/ui-taskbar/src/lib/template.ts
@@ -1,4 +1,4 @@
-import { html, TemplateResult } from 'lit-element'
+import { html, TemplateResult } from 'lit'
 import { UiTaskbarComponent } from './component'
 
 export default function template(this: UiTaskbarComponent): TemplateResult {
