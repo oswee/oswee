daf6d45409bd4a21f7246d1a52fcf7d97cb95e14
Dzintars Klavins <dzintars.klavins@gmail.com>
2022-05-20 20:54:23 +0300

    feat(nvim): fix yamlls config

--- ansible/custom/roles/nvim/files/lua/modules/lsp/opts/yamlls.lua
+++ ansible/custom/roles/nvim/files/lua/modules/lsp/opts/yamlls.lua
@@ -1,27 +1,46 @@
+-- Server settings: https://github.com/redhat-developer/yaml-language-server#language-server-settings
 return {
-  -- cmd = { "yaml-language-server", "--stdio" },
-  -- cmd = { "yamllint", "--strict", "--format parsable ${INPUT}" },
+  cmd = { 'yaml-language-server', '--stdio' },
   filetypes = {
+    'helm',
     'yaml',
-    'yaml.docker-compose',
     'yaml.ansible',
+    'yaml.docker-compose',
   },
   single_file_support = true,
-  completion_menu_label = 'YAML',
   settings = {
+    editor = {
+      tabSize = 2,
+    },
     yaml = {
-      rootMarkers = {'.yamllint.yaml'},
       hover = true,
       completion = true,
+      format = {
+        enable = false,
+        singleQuote = true,
+        bracketSpacing = true,
+      },
       validate = true,
       schemaStore = {
-        -- enable = true,
-        -- url = "https://www.schemastore.org/api/json/catalog.json",
-        url = '',
-        enable = false,
+        enable = true,
+        url = 'https://www.schemastore.org/api/json/catalog.json',
       },
       schemas = {
-        ['https://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yaml,yml}',
+        -- ['https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json'] = '/*.k8s.yaml',
+        kubernetes = {
+          '*.k8s.yaml',
+        },
+        ['https://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
+        ['https://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+        ['https://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+        ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
+        ['https://json.schemastore.org/github-workflow'] = '.github/workflows/*.{yml,yaml}',
+        ['https://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+        ['https://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+        ['https://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+        ['https://json.schemastore.org/gitlab-ci'] = '*gitlab-ci*.{yml,yaml}',
+        ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose.{yml,yaml}',
+        ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}'
       },
     },
     redhat = {
