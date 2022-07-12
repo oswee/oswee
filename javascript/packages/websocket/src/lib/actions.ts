d40a77d1c6ad2835363af68aff4d7c9133e73b23
Dzintars Klavins <dzintars.klavins@gmail.com>
2022-07-12 15:15:42 +0300

    feat(websocket): update imports and formatting

--- javascript/packages/websocket/src/lib/actions.ts
+++ javascript/packages/websocket/src/lib/actions.ts
@@ -1,15 +1,14 @@
 import { WebsocketTypes } from './constants'
-import { ActionsUnion, createAction, StringMap } from '@oswee/modules/action'
+import { ActionsUnion, createAction } from '@oswee/modules/action'
 import { IConnectPayload } from './contracts'
 
 export const WebsocketActions = {
-  websocketConnect: (payload: IConnectPayload) =>
-    createAction(WebsocketTypes.CONNECT, payload),
-  websocketConnecting: () => createAction(WebsocketTypes.CONNECTING),
-  websocketConnected: () => createAction(WebsocketTypes.CONNECTED),
-  websocketDisconnect: () => createAction(WebsocketTypes.DISCONNECT),
-  websocketDisconnected: () => createAction(WebsocketTypes.DISCONNECTED),
-  websocketError: () => createAction(WebsocketTypes.ERROR),
+	websocketConnect: (payload: IConnectPayload) => createAction(WebsocketTypes.CONNECT, payload),
+	websocketConnecting: () => createAction(WebsocketTypes.CONNECTING),
+	websocketConnected: () => createAction(WebsocketTypes.CONNECTED),
+	websocketDisconnect: () => createAction(WebsocketTypes.DISCONNECT),
+	websocketDisconnected: () => createAction(WebsocketTypes.DISCONNECTED),
+	websocketError: () => createAction(WebsocketTypes.ERROR),
 }
 
 export type WebsocketActionsUnion = ActionsUnion<typeof WebsocketActions>
