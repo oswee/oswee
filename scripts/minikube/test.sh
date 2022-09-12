41e5629a3cf9ab120ca2d9e5bedd92e7fbe841b1
Dzintars Klavins <dzintars.klavins@gmail.com>
2022-09-12 19:53:56 +0300

    feat(minikube): add minikube script file. wip

---
+++ scripts/minikube/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Server uptime is:" $(uptime)
+
+# Install the metrics server
+
+# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
