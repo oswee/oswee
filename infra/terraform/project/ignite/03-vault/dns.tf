1917db9dc720f42d106b9572be63093af8cdd93a
Dzintars Klavins <dzintars.klavins@gmail.com>
2022-11-14 18:13:42 +0200

    chore(terraform): remove old ignite.vault module

--- infra/terraform/project/ignite/03-vault/dns.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "vault" {
-  zone_id = data.terraform_remote_state.dns.outputs.root_zone_id
-  name    = var.instance.hostname
-  value   = var.public_ip_address
-  type    = "A"
-  ttl     = 1
-  proxied = false
-}
