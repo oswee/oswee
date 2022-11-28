abe497573194b95fd08e86537a328adf33861983
Dzintars Klavins <dzintars.klavins@gmail.com>
2022-11-28 18:09:12 +0200

    chore(k8s): update sonarqube script

--- k8s/containers/sonarqube/volumes.sh
+++ k8s/containers/sonarqube/volumes.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!/bin/nu
 
-mkdir -p /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/postgres
-mkdir -p /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/postgres/data
-mkdir -p /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/sonarqube/data
-mkdir -p /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/sonarqube/logs
-mkdir -p /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/sonarqube/extensions
+sudo rm -r /home/dzintars/containers/github.com/dzintars/oswee/sonarqube
+
+mkdir /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/postgres
+mkdir /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/postgres/data
+mkdir /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/sonarqube/data
+mkdir /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/sonarqube/logs
+mkdir /home/dzintars/containers/github.com/dzintars/oswee/sonarqube/volumes/sonarqube/extensions
 
 # sudo firewall-cmd --add-port 9100/tcp --permanent
 # sudo firewall-cmd --reload
 
-podman play kube pod.yaml
-podman logs -f sonarqube-server
+# Increase memory for ElasticSearch
+# sudo sysctl -w vm.max_map_count=262144
+# sudo sysctl --system
+
+# podman play kube pod.yaml
+# podman logs -f sonarqube-server
