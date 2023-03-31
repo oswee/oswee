c8ba53c365046cfb60eb1b955b7a3c4c8813145f
Dzintars Klavins <dzintars.klavins@gmail.com>
2023-03-31 12:27:57 +0300

    chore(packer): add test script

---
+++ infra/packer/scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export BUILD_USERNAME
+export BUILD_KEY
+
+echo "Testing some script....................."
+echo $BUILD_USERNAME
+echo $BUILD_KEY
+echo "End testing some script....................."
