8add6df1df8d37ba502591442110e3652d7ca93e
Dzintars Klavins <dzintars.klavins@gmail.com>
2023-03-31 12:28:20 +0300

    feat(packer): update wrapper script

--- infra/packer/build.sh
+++ infra/packer/build.sh
@@ -17,13 +17,71 @@ follow_link() {
 }
 
 # TODO: How to abstract home path?
-VAGRANT_BOX_PATH=/home/dzintars/vagrant/boxes
+VAGRANT_BOX_PATH=/media/registry/vagrant/boxes
 SCRIPT_PATH=$(realpath "$(dirname "$(follow_link "$0")")")
 CONFIG_PATH=$(realpath "${1:-${SCRIPT_PATH}/config}")
 
 mkdir -p "$VAGRANT_BOX_PATH"
 
 menu_option_1() {
+  INPUT_PATH="$SCRIPT_PATH"/builds/base-image
+  echo -e "\nCONFIRM: Build a Base Image Template for QEMU?"
+  echo -e "\nContinue? (y/n)"
+  read -r REPLY
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    exit 1
+  fi
+
+  echo "Building a Base Image Template for QEMU..."
+
+  echo "Initializing HashiCorp Packer and required plugins..."
+  packer init "$INPUT_PATH"
+
+  echo "Starting the HashiCorp Packer build..."
+  PKR_VAR_VAGRANT_BOX_PATH=$VAGRANT_BOX_PATH \
+  # packer build -only=qemu.* -force \
+  packer build -force \
+      -var-file="$CONFIG_PATH/qemu.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/build.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/common.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/ansible.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/vagrant.pkrvars.hcl" \
+      "$INPUT_PATH"
+
+  echo "Done."
+}
+
+menu_option_2() {
+  INPUT_PATH="$SCRIPT_PATH"/builds/ci-image
+  echo -e "\nCONFIRM: Build a CI Image Template for QEMU?"
+  echo -e "\nContinue? (y/n)"
+  read -r REPLY
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    exit 1
+  fi
+
+  echo "Building a CI Image Template for QEMU..."
+
+  ### Initialize HashiCorp Packer and required plugins. ###
+  echo "Initializing HashiCorp Packer and required plugins..."
+  packer init "$INPUT_PATH"
+
+  echo "Starting the HashiCorp Packer build..."
+  PKR_VAR_VAGRANT_BOX_PATH=$VAGRANT_BOX_PATH \
+  # packer build -only=qemu.* -force \
+  packer build -force \
+      -var-file="$CONFIG_PATH/qemu.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/build.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/common.pkrvars.hcl" \
+      -var-file="$CONFIG_PATH/ansible.pkrvars.hcl" \
+      "$INPUT_PATH"
+
+  echo "Done."
+}
+
+menu_option_3() {
   INPUT_PATH="$SCRIPT_PATH"/builds/fedora/Server-netinst-35
   echo -e "\nCONFIRM: Build a Fedora 35 Server-netinst Template for QEMU?"
   echo -e "\nContinue? (y/n)"
@@ -53,7 +111,7 @@ menu_option_1() {
   echo "Done."
 }
 
-menu_option_2() {
+menu_option_4() {
   INPUT_PATH="$SCRIPT_PATH"/builds/fedora/Server-netinst-37
   echo -e "\nCONFIRM: Build a Fedora 37 Server-netinst Template for QEMU?"
   echo -e "\nContinue? (y/n)"
@@ -71,35 +129,6 @@ menu_option_2() {
   packer init "$INPUT_PATH"
 
   ### Start the HashiCorp Packer Build ###
-  echo "Starting the HashiCorp Packer build..."
-  PKR_VAR_VAGRANT_BOX_PATH=$VAGRANT_BOX_PATH \
-  # packer build -only=qemu.* -force \
-  packer build -force \
-      -var-file="$CONFIG_PATH/qemu.pkrvars.hcl" \
-      -var-file="$CONFIG_PATH/build.pkrvars.hcl" \
-      -var-file="$CONFIG_PATH/common.pkrvars.hcl" \
-      -var-file="$CONFIG_PATH/ansible.pkrvars.hcl" \
-      "$INPUT_PATH"
-
-  echo "Done."
-}
-
-menu_option_3() {
-  INPUT_PATH="$SCRIPT_PATH"/builds/fedora/Server
-  echo -e "\nCONFIRM: Build a Fedora Server Template for QEMU?"
-  echo -e "\nContinue? (y/n)"
-  read -r REPLY
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
-  then
-    exit 1
-  fi
-
-  echo "Building a Fedora Server Template for QEMU..."
-
-  ### Initialize HashiCorp Packer and required plugins. ###
-  echo "Initializing HashiCorp Packer and required plugins..."
-  packer init "$INPUT_PATH"
-
   echo "Starting the HashiCorp Packer build..."
   PKR_VAR_VAGRANT_BOX_PATH=$VAGRANT_BOX_PATH \
   # packer build -only=qemu.* -force \
@@ -131,9 +160,10 @@ until [ "$selection" = "0" ]; do
   echo ""
   echo "      Linux Distribution:"
   echo ""
-  echo "        1  -  Fedora 35 Server-netinst (QEMU)"
-  echo "        2  -  Fedora 37 Server-netinst (QEMU)"
-  echo "        3  -  Fedora Server (QEMU)"
+  echo "        1  -  Base Image (QEMU)"
+  echo "        2  -  CI Image (QEMU)"
+  echo "        3  -  Fedora 35 Server-netinst (QEMU)"
+  echo "        4  -  Fedora 37 Server-netinst (QEMU)"
   echo ""
   echo "      Other:"
   echo ""
@@ -145,6 +175,7 @@ until [ "$selection" = "0" ]; do
     1 ) clear ; menu_option_1 ; press_enter ;;
     2 ) clear ; menu_option_2 ; press_enter ;;
     3 ) clear ; menu_option_3 ; press_enter ;;
+    4 ) clear ; menu_option_4 ; press_enter ;;
     q ) clear ; exit ;;
     * ) clear ; incorrect_selection ; press_enter ;;
   esac
