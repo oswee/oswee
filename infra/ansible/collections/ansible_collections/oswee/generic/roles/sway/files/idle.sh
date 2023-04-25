550fb7947ce130dc5aec447da6a0aa6e18038344
Dzintars Klavins <dzintars.klavins@gmail.com>
2023-04-25 11:04:47 +0300

    chore(ansible): cleanup generic.sway role

--- infra/ansible/collections/ansible_collections/oswee/generic/roles/sway/files/idle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-swayidle -w -d \
-	timeout 600 '~/.config/sway/lock.sh -f --grace 10 --fade-in 8' \
-	# timeout 900 'swaymsg "output * dpms off"' \
-	# resume 'swaymsg "output * dpms on"; sleep 2; swaymsg "output * enable"' \
-	# before-sleep 'pgrep swaylock || ~/.config/sway/lock.sh -f --fade-in 0 && true'
-
